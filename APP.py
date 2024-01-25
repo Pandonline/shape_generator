302c2f85e0292b6df7235e477e98ed9cc6d12231
Pandadorable <geffroy250@gmail.com>
2024-01-25 11:11:07 +0100

    Update APP.py
    /!\ crash with double click in spinbox

--- APP.py
+++ APP.py
@@ -2,7 +2,6 @@ from tkinter import *
 import form
 
 LIST_MODE = ['Square','Rectangle','Circle','Disk']
-MODE = LIST_MODE[0]
 
 # MAIN FRAME
 m=Tk()
@@ -16,18 +15,7 @@ c.grid(row=0,column=0)
 menu = Frame(w)
 # SELECTE MODE
 def ModeSelect(evt):
-    MODE = Lb.get(Lb.curselection())
-    HideAllSizeFrame(evt)
-    match(MODE):
-        case 'Circle' :
-            circle_frame.grid()
-        case 'Disk' :
-            circle_frame.grid()
-        case 'Square':
-            square_frame.grid()
-        case 'Rectangle':
-            rectangle_frame.grid()
-        
+    build_alt_frame(evt)
 
 Lb = Listbox(menu)
 for mode in LIST_MODE:
@@ -35,79 +23,43 @@ for mode in LIST_MODE:
 Lb.select_set(0)
 Lb.bind('<<ListboxSelect>>',ModeSelect)
 Lb.grid()
-    
-def HideAllSizeFrame(evt):
-    circle_frame.grid_forget()
-    square_frame.grid_forget()
-    rectangle_frame.grid_forget()
+
 
 # ALTERNATIVE FRAMES FOR SIZE
-form_dimension_1 = IntVar()
-form_dimension_2 = IntVar()
-def print_val_size():
-    print(form_dimension_1.get())
-    print(form_dimension_2.get())
-    print()
-    
-# Validating function
-def validate(user_input):
-    # check if the input is numeric
-    if  user_input.isdigit():
-        # Fetching minimum and maximum value of the spinbox
-        minval = 0
-        maxval = 150
- 
-        # check if the number is within the range
-        if int(user_input) not in range(minval, maxval):
-            print ("Out of range")
-            return False
- 
-        # Printing the user input to the console
-        print(user_input)
-        return True
- 
-    # if input is blank string
-    elif user_input is "":
-        print(user_input)
-        return True
- 
-    # return false is input is not numeric
-    else:
-        print("Not numeric")
-        return False
+def print_ssinbox() :
+    print(spinvar_1.get())
+    print(spinvar_2.get())
 
-### CIRCLE FRAME
-circle_frame = Frame(menu)
-circle_label = Label(circle_frame,text="ray :")
-circle_label.grid()
-circle_ray = Spinbox(circle_frame, from_=0, to=100,textvariable=form_dimension_1)
-circle_ray.grid()
-circle_ray.configure(command=print_val_size)
-
-### SQUARE FRAME
-square_frame = Frame(menu)
-square_label = Label(square_frame,text="width :")
-square_label.grid()
-square_width = Spinbox(square_frame, from_=0, to=100,textvariable=form_dimension_1)
-square_width.grid()
-square_width.configure(command=print_val_size)
-
-### RECTANGLE FRAME
-rectangle_frame = Frame(menu)
-rectangle_label = Label(rectangle_frame,text="width :")
-rectangle_label.grid()
-rectangle_width = Spinbox(rectangle_frame, from_=0, to=100,textvariable=form_dimension_1)
-rectangle_width.grid()
-rectangle_width.configure(command=print_val_size)
-rectangle_label_2 = Label(rectangle_frame,text="lenght :")
-rectangle_label_2.grid()
-rectangle_lenght = Spinbox(rectangle_frame, from_=0, to=100,textvariable=form_dimension_2)
-rectangle_lenght.grid()
-rectangle_lenght.configure(command=print_val_size)
-
-
-#DEFAULT ALTERNATIVE FRAME
-square_frame.grid()
+alt_frame = Frame(menu)
+spinvar_1 = IntVar()
+spinvar_2 = IntVar()
+def build_alt_frame(evt):
+    for widget in alt_frame.winfo_children():
+        widget.destroy()
+    alt_label_1 = Label(alt_frame,text="dim_1 :")
+    mode = Lb.get(Lb.curselection())
+    match(mode):
+        case 'Circle' :
+            alt_label_1 = Label(alt_frame,text="ray :")
+        case 'Disk' :
+            alt_label_1 = Label(alt_frame,text="ray :")
+        case 'Square':
+            alt_label_1 = Label(alt_frame,text="width :")
+        case 'Rectangle':
+            alt_label_1 = Label(alt_frame,text="width :")
+    alt_label_1.grid()
+    alt_box_1 = Spinbox(alt_frame, from_=0, to=100, textvariable=spinvar_1,wrap=True)
+    alt_box_1.grid()
+    if(mode == 'Rectangle'):
+        alt_label_2 = Label(alt_frame,text="lenght :")
+        alt_label_2.grid()
+        alt_box_2 = Spinbox(alt_frame, from_=0, to=100, textvariable=spinvar_2,wrap=True)
+        alt_box_2.grid()
+    alt_refresh = Button(alt_frame,text="generate",command=print_ssinbox)
+    alt_refresh.grid()
+    alt_frame.grid()
+build_alt_frame(None)
+alt_frame.grid()
 
 # END OF WINDOW
 menu.grid(row=0,column=1,sticky="N")
